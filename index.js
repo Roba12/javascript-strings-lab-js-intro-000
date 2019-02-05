f3b392822964df743c0b785de12707fba3b0bea2
Hashim Ahmed <hashimko12@hotmail.com>
2019-02-05 04:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = ""greating + specialGuest + "!";
+var greetSpecialGuest = greating + specialGuest + "!";
 
 var topic = "space";
 
